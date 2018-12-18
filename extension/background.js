ebb35f6a27b03b5d725f3284b06b01bd82aba292
mattc0llier <matt.collier@hotmail.com>
2018-12-18 23:15:41 +0000

    Chrome extension now posting to the server on image click and loading of page

--- extension/background.js
+++ extension/background.js
@@ -5,6 +5,34 @@ chrome.browserAction.onClicked.addListener(buttonClicked)
 function buttonClicked(tab){
   chrome.tabs.captureVisibleTab(null, {}, function (image) {
    // You can add that image HTML5 canvas, or Element.
-   console.log({url: tab.url, title: tab.title, image: image});
-  });
+   const contextData = { url: tab.url, title: tab.title/*, image: image*/ }
+   console.log(contextData);
+
+   fetch('http://localhost:8080/api/context', {
+      method: 'post',
+      body: JSON.stringify(contextData),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: "cors"
+    }).then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+
+ });
 }
+
+chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
+
+ if(changeInfo.status == "loading"){
+
+    console.log(tab);
+
+     var request = new XMLHttpRequest();
+
+     request.open("POST", "http://localhost:8080/api/context", true);
+     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+     request.send(JSON.stringify({denny: 1, mark: 1, johnny: 0}));
+   }
+ });
