c213e12cb26ea35760a6b2fadb6519c7d74858aa
mattc0llier <matt.collier@hotmail.com>
2018-12-18 15:04:25 +0000

    Added get and post routes to server and added nodemon to start script

--- server.js
+++ server.js
@@ -6,9 +6,42 @@ app.use(bodyParser.json());
 app.use('/static', express.static('static'));
 app.set('view engine', 'hbs');
 
+const storage = {
+    contexts : {
+      1: {
+        image: "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD",
+        title: "How do I run a Server in chrome extension? - Quora",
+        url: "https://www.quora.com/How-do-I-run-a-Server-in-chrome-extension",
+      },
+      2: {
+        image: "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD",
+        title: "webpack bundle.js not found - Stack Overflow",
+        url: "https://stackoverflow.com/questions/33502987/webpack-bundle-js-not-found"
+      }
+    }
+};
+
+function getContext(){
+    return storage.contexts;
+}
+
+function getStudentById(studentId){
+    return storage.contexts[studentId];
+}
+
 app.get('/', function(req, res){
   res.render('index');
 });
+
+app.get('/context', function(req, res){
+  const contexts = getContext();
+  res.json(contexts);
+});
+
+app.post('/api/context', (req, res) => {
+  res.json(req.body);
+  console.log(req.body);
+})
 
 app.listen(8080, function(){
   console.log('Listening on port 8080');
